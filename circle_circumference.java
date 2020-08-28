5085e7f182d83f7a065485bcdc13d6f3b6d17fce
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-28 19:16:47 +0530

    Add files via upload

---
+++ circle_circumference.java
@@ -0,0 +1,14 @@
+package pack_1;
+import java.util.*;
+
+public class circle_circumference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter radius:");
+		float r=obj.nextFloat();
+		float c=(float)(2*3.14*r);
+	    System.out.println("circumference of circle:="+c);
+	}
+}
