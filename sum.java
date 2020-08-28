933b8cea7b21629c0a0ab95bfa180c28c6f100da
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-28 19:09:41 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,16 @@
+package pack_1;
+import java.util.*;
+public class sum {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter 2 values:");
+		int a=obj.nextInt();
+		int b=obj.nextInt();
+		int c=a+b;
+		System.out.println("sum value="+c);
+		
+
+	}
+
+}
