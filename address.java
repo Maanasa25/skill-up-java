9d66337f858d77670c188c7f41baf0e4c268c7ae
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-29 09:14:17 +0530

    Add files via upload

---
+++ address.java
@@ -0,0 +1,17 @@
+package pack_1;
+
+public class address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="maanasa";
+		String hno="11-6-4";
+		String city="peddapuram";
+		System.out.println("name="+name);
+		System.out.println("hno="+hno);
+	    System.out.println("city="+city);
+
+
+	}
+
+}
