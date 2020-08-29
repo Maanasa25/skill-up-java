6175714cd4cf5929bc2d442e162693a00d8254fa
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-29 17:57:00 +0530

    Add files via upload

---
+++ convert.java
@@ -0,0 +1,15 @@
+package pack_1;
+
+public class convert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b=10;
+		int i=b;
+		System.out.println("b value="+b);
+		System.out.println("i value="+i);
+	
+
+	}
+
+}
