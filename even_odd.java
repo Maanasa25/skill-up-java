9878bb64f43d9a43accd2a90cb1e2f784bb8bfac
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-29 17:55:53 +0530

    Add files via upload

---
+++ even_odd.java
@@ -0,0 +1,20 @@
+package pack_2;
+import java.util.*;
+
+public class even_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter number:");
+		int x=obj.nextInt();
+		if(x%2==0)
+			System.out.println("even number="+x);
+		else
+			System.out.println("odd number="+x);
+		
+		
+
+	}
+
+}
