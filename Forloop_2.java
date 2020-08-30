b4eed4d797e53ddd9c2ae6082deb71211490e3f6
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-30 18:50:09 +0530

    Add files via upload

---
+++ Forloop_2.java
@@ -0,0 +1,15 @@
+package pack_2;
+import java.util.*;
+
+public class Forloop_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
