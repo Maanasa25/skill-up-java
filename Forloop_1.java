ff17cda510dea4636d0960ded5c7a48c7c5ad283
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-30 18:49:35 +0530

    Add files via upload

---
+++ Forloop_1.java
@@ -0,0 +1,16 @@
+package pack_2;
+import java.util.*;
+
+public class Forloop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number from 1 to 10:");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
