465fb06ecb1c66f2ee7c908edf256574e37e65fe
Maanasa25 <70372129+Maanasa25@users.noreply.github.com>
2020-08-30 18:49:08 +0530

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,24 @@
+package pack_2;
+
+public class matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[][]={
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		System.out.println("array values:");
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(a[i][j]+"");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
